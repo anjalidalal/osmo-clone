1c93ecebb37f4e113f21ee5fde60fc27bfac4cc6
anjalidalal <anjalimaria2002@gmail.com>
2025-03-25 14:56:45 +0530

    updated read me

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { AccessToken } from 'livekit-server-sdk';
-import { apiKey, apiSecret, wsUrl } from '@/app/components/utils';
+import { apiKey, apiSecret, wsUrl } from './../../components/utils';
 
 // Do not cache endpoint result
 export const revalidate = 0;
